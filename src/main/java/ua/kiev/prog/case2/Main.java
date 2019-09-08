8070dfe8101714e55e6d3c9c9be5878ca7c07fb7
unknown <gworkout@ukr.net>
2019-09-08 20:35:36 +0300

    first commit

---
+++ src/main/java/ua/kiev/prog/case2/Main.java
@@ -0,0 +1,44 @@
+package ua.kiev.prog.case2;
+
+import ua.kiev.prog.shared.Client;
+import ua.kiev.prog.shared.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/mydb?serverTimezone=UTC";
+    static final String DB_USER = "root";
+    static final String DB_PASSWORD = "asDF1506";
+
+    public static void main(String[] args) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+
+        ConnectionFactory factory = new ConnectionFactory(
+                DB_CONNECTION, DB_USER, DB_PASSWORD
+        );
+
+        Connection conn = factory.getConnection();
+        try {
+            ClientDAOEx dao = new ClientDAOEx(conn, "clients");
+
+            Client c = new Client("test", 1);
+            dao.add(c);
+
+            List<Client> list = dao.getAll(Client.class);
+            for (Client cli : list)
+                System.out.println(cli);
+
+            Client cl1 = list.get(0);
+            cl1.setAge(34);
+            dao.update(cl1);
+
+           dao.delete(list.get(0));
+        } finally {
+            sc.close();
+            if (conn != null) conn.close();
+        }
+    }
+}
